66947f7a36f8eba424a95ee00c5dd12c08ebcec8
mz-hanb <hanb@mz.co.kr>
2018-04-04 14:46:34 +0900

    complete practice

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log( 'hellow::::' );
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import App from './src/App'; 
+import typography from './typography';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import styles from './styles.scss';
+
+
+export default () =>
+<div className={styles.title}>
+  <h1>This is Header</h1>
+  <p>lorem ipsum</p>
+</div>;
